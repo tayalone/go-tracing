a90d3d5a05dce3fc22e5ec8f3458b6d5f4028b73
Panupong Rong-O <tayalone@gmail.com>
2022-12-11 15:03:29 +0700

    ➕ add otelgin

--- api/main.go
+++ api/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tayalone/go-trancing/api/trancer"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	// / setup Otel Trancer ---
+	_, err := trancer.JaegertracerProvider(os.Getenv("JEAGER_ENDPOINT"), os.Getenv("SERVICE_NAME"), os.Getenv("ENVIROMENT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Otel Trancer Loonking Good !!!")
+	// / -----------------------
 
+	r := gin.Default()
+	r.Use(otelgin.Middleware(os.Getenv("SERVICE_NAME"))) // <- add Otel Middleware
+
+	r.GET("/ping", func(c *gin.Context) {
 		time.Sleep(500 * time.Millisecond) /* delay 0.5 secs */
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
