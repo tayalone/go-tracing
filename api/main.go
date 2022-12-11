fd86618465a94fa218c007e8bfe50710478f8439
Panupong Rong-O <tayalone@gmail.com>
2022-12-11 18:00:07 +0700

    ✨ add span doene

--- api/main.go
+++ api/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	// / setup Otel Trancer ---
-	_, err := trancer.JaegertracerProvider(os.Getenv("JEAGER_ENDPOINT"), os.Getenv("SERVICE_NAME"), os.Getenv("ENVIROMENT"))
+	tp, err := trancer.JaegertracerProvider(os.Getenv("JEAGER_ENDPOINT"), os.Getenv("SERVICE_NAME"), os.Getenv("ENVIROMENT"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,5 +29,28 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/test-trancing-1", func(ctx *gin.Context) {
+		time.Sleep(50 * time.Millisecond)
+
+		tr := tp.Tracer("/testxtrancing-1")
+
+		// / do task no1
+		_, span1 := tr.Start(ctx.Request.Context(), "task-1")
+		time.Sleep(150 * time.Millisecond)
+		span1.End()
+
+		// / do task no2
+		_, span2 := tr.Start(ctx.Request.Context(), "task-2")
+		time.Sleep(150 * time.Millisecond)
+		span2.End()
+
+		time.Sleep(100 * time.Millisecond)
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
